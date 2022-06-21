1aa580f7e5f4302fa95e4483b87b1554ce73ad0a
Kleverson Eller <kleverson.a16@gmail.com>
2022-06-21 20:34:11 -0300

    :sparkles: feat: Requisito - 3: Executando um POST para a rota '/login'

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
 const talkerRouter = require('./routes/talkerRouter');
 
 const app = express();
@@ -11,6 +12,21 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.post('/login', (req, res) => {
+  try {
+      const { email, password } = req.body;
+
+      if ([email, password].includes(undefined)) {
+        return res.status(401).json({ message: 'missing fields' }); 
+}
+      const token = crypto.randomBytes(8).toString('hex');
+
+      return res.status(200).json({ token });
+  } catch (error) {
+      res.status(500).end();
+  }
 });
 
 app.use('/talker', talkerRouter);
