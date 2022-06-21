602c3b800fd6be9b9da17bf0fdc91e9c29b71a38
Kleverson Eller <kleverson.a16@gmail.com>
2022-06-21 20:26:18 -0300

    :sparkles: feat: Requisito - 2: Executando um GET para a rota '/talker/:id'

--- routes/talkerRouter.js
+++ routes/talkerRouter.js
@@ -11,4 +11,15 @@ router.get('/', async (_req, res) => {
     return res.status(200).send(result);
 });
 
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const result = await readFile(file);
+    const talker = result.find((objeto) => objeto.id === +id);
+
+    if (!talker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+
+    return res.status(200).send(talker);
+});
+
 module.exports = router;
