bf9d912f6323ce326ea70406f1fe6c8e41909ecf
Kleverson Eller <kleverson.a16@gmail.com>
2022-06-21 21:04:47 -0300

    :sparkles: feat: Requisito - 4: Implementando validaçoes para o POST da rota '/login'

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const crypto = require('crypto');
 const talkerRouter = require('./routes/talkerRouter');
+const validLogin = require('./middleware/validLogin');
 
 const app = express();
 app.use(bodyParser.json());
@@ -14,19 +15,9 @@ app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
 
-app.post('/login', (req, res) => {
-  try {
-      const { email, password } = req.body;
-
-      if ([email, password].includes(undefined)) {
-        return res.status(401).json({ message: 'missing fields' }); 
-}
-      const token = crypto.randomBytes(8).toString('hex');
-
-      return res.status(200).json({ token });
-  } catch (error) {
-      res.status(500).end();
-  }
+app.post('/login', validLogin, (_req, res) => {
+  const token = crypto.randomBytes(8).toString('hex');
+  return res.status(200).json({ token });
 });
 
 app.use('/talker', talkerRouter);
