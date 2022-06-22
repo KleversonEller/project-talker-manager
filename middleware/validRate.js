2c9f68d400bceedb7821a3649892ad65c0bb9cc4
Kleverson Eller <kleverson.a16@gmail.com>
2022-06-22 03:06:29 -0300

    :sparkles: feat: Middleware: Criando validação do campo 'rate' da rota '/talker'

---
+++ middleware/validRate.js
@@ -0,0 +1,18 @@
+module.exports = (req, res, next) => {
+    try {
+        const { talk: { rate } } = req.body;
+
+        if (!rate) {
+            return res.status(400).json({ message: 'O campo "rate" é obrigatório' });
+        }
+
+        if (rate < 1 || rate > 5) {
+            return res.status(400)
+            .json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
+        }
+
+        return next();
+    } catch (error) {
+        return res.status(500).end();
+    }
+};
